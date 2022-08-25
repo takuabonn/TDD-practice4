f07988072a66ead2239ab04d193043deae797e8b
takuabonn <k.takumi19980322@gmail.com>
2022-08-25 21:45:03 +0900

    [refactor]: 持ち時間TまでにN部屋まで辿り着けるかの判定

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
 export default class Explore {
+  private roomAmount: number;
+  private bonusRoomAmount: number;
+  private havingTime: number;
+  private consumptionTimeArry: number[];
+  private bonusRoomAndTimeOb: { [key: number]: number };
+
+  constructor(
+    roomAmount: number,
+    bonusRoomAmount: number,
+    havingTime: number,
+    consumptionTimeArry: number[],
+    bonusRoomAndTimeOb: { [key: number]: number }
+  ) {
+    this.roomAmount = roomAmount;
+    this.bonusRoomAmount = bonusRoomAmount;
+    this.havingTime = havingTime;
+    this.consumptionTimeArry = consumptionTimeArry;
+    this.bonusRoomAndTimeOb = bonusRoomAndTimeOb;
+  }
   canGoal = () => {
-    return true;
+    let canGoal = true;
+    for (let i = 2; i <= this.roomAmount; i++) {
+      this.havingTime -= this.consumptionTimeArry[i - 2];
+      if (this.havingTime <= 0) {
+        canGoal = false;
+        break;
+      }
+
+      if (this.bonusRoomAndTimeOb[i]) {
+        this.havingTime += this.bonusRoomAndTimeOb[i];
+      }
+    }
+    return canGoal;
   };
 }
